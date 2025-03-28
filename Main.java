13dbc74b142ad1677d3b4f48b16be80113583dc3
TulioAmaralFx <167933712+TulioAmaralFx@users.noreply.github.com>
2025-03-28 12:16:19 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.List;
+import java.lang.Thread;
+
 
 class Banco{
     private String cpf;
@@ -23,9 +19,24 @@ class Banco{
     public double getDinheiro(){
         return dinheiro;
     }
+    /*public void setSacar(){
+        this.dinheiro = dinheiro;
+    }*/
+    public boolean Sacar(double valor){
+        if(valor > 0 && dinheiro >= valor){
+            this.dinheiro -= valor;
+            return true;
+        }
+        return false;
+    }
+    public void Depositar(double valor){
+        if(valor > 0){
+            this.dinheiro += valor;
+        }
+    }
     
     public String toString(){
-        return "Cpf" + cpf + "\n R$ " + dinheiro;
+        return "Cpf " + cpf + " R$ " + dinheiro;
     }
 }
 
@@ -36,10 +47,12 @@ public class Main
 	
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 	    listaBanco.add(new Banco("00011122233", 10000));
+	    listaBanco.add(new Banco("03981119100", 10000));
 	    
 	    boolean sair = false;
+	    
 	    
 		while(!sair){
 		    exibirMenu();
@@ -49,15 +62,21 @@ public class Main
 		    switch(opcao){
 		        case 1:
 		            inserirUsuario();
+		            Thread.sleep(2000);
 		            break;
 		        case 2:
 		            listarUsuarios();
+		            Thread.sleep(2000);
+		            System.out.println("Para continuar aperte enter: ");
+		            String esperar = scanner.nextLine();
 		            break;
 		        case 3:
 		            transferencia();
+		            Thread.sleep(2000);
 		            break;
 		        case 4:
 		            excluirUsuario();
+		            Thread.sleep(2000);
 		            break;
 		        case 5:
 		            System.out.println("Saindo!!!");
@@ -71,8 +90,9 @@ public class Main
 		
 		
 	}
-	public static void exibirMenu(){
-        System.out.println("Banco\n");
+	
+	public static void exibirMenu(){ // EXIBIR MENU
+        System.out.println("******Banco*******\n");
         System.out.println("1. Inserir um usuario\n");
         System.out.println("2. Listar usuarios\n");
         System.out.println("3. Transferencia de valor\n");
@@ -80,7 +100,8 @@ public class Main
         System.out.println("5. Sair do programa\n");
 
     }
-    public static void inserirUsuario(){
+    
+    public static void inserirUsuario(){ // INSERIR USUARIO
         System.out.println("Insira o nome do usuario: ");
         String cpf = scanner.nextLine();
         
@@ -100,36 +121,74 @@ public class Main
         listaBanco.add(new Banco(cpf, dinheiro));
         System.out.println("\nUsuario e valor adicionados com sucesso\n");
     }
-    public static void listarUsuarios(){
+    
+    public static void listarUsuarios(){ // LISTAR USUARIOS
         System.out.println("\nLista dos usuarios\n");
         
         if(listaBanco.isEmpty()){
             System.out.println("\nLista de usuarios esta vazia\n");
+            
         }else{
-            for(int i = 0; i <= listaBanco.size(); i++){
-                System.out.println((i + 1) + " " + listaBanco.get(i));
+            
+            for(int i = 0; i <= listaBanco.size() - 1; i++){
+                System.out.println((i + 1) + ". " + listaBanco.get(i));
             }
+            
         }
+        
     }
-    public static void transferencia(){
+    
+    private static void transferencia(){ // TRANSFERENCIA
         listarUsuarios();
         
-        System.out.println("\nDigite o numero do usuario: ");
-        int usuarioUm = scanner.nextInt();
+        System.out.println("\nDigite o cpf do remetente: ");
+        String cpfUm = scanner.nextLine();
         
-        System.out.println("\nDigite o numero do usuario: \n");
-        int usuarioUm = scanner.nextInt();
         
-        String userUm = listaBanco.get(usuarioUm);
-        String userDois = listaBanco.get(usuarioDois);
         
-        if(listaBanco){
+        System.out.println("\nDigite o cpf do destinatario: \n");
+        String cpfDois = scanner.nextLine();
+        
+        System.out.println("\nDigite o valor de transferencia: ");
+        double valor = scanner.nextDouble();
+        Banco remetente = null, destinatario = null;
+        
+        for(Banco a: listaBanco){
+            if(a.getCpf().equals(cpfUm)){
+                remetente = a;
+            }
+            if(a.getCpf().equals(cpfDois)){
+                destinatario = a;
+            }
+        }
+        
+        if(remetente == null || destinatario == null){
+            System.out.println("CPF nao encontrado");
+        }else{
+            if(remetente.Sacar(valor)){
+                destinatario.Depositar(valor);
+                System.out.println("Transferencia realizada com sucesso!");
+            }else{
+                System.out.println("Saldo insuficiente!!");
+            }
             
         }
             
-        }
     }
+    
     public static void excluirUsuario(){
-        //
+        listarUsuarios();
+        
+        System.out.println("Digite o numero do usuario: ");
+        int excluir = scanner.nextInt();
+        scanner.nextLine();
+        if (excluir > 0 && excluir <= listaBanco.size()) {
+            listaBanco.remove(excluir - 1); // Remove pelo índice correto
+            System.out.println("Usuário removido com sucesso!");
+        } else {
+            System.out.println("Número inválido! Tente novamente.");
+        }
+            
     }
+    
 }
